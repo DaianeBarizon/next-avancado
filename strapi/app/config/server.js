cfb80e52b0bfc019f0ea7e126f18be9b3832687c
DaianeBarizon <daianebarizon22@gmail.com>
2021-07-04 10:44:26 -0300

    feat: insert strapi

---
+++ strapi/app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'eabc99cd9785c34d27556af551796669'),
+    },
+  },
+});
---
+++ strapi/app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fe6ade3f-89fa-41a6-8264-74b3b6a09afe'
+};
